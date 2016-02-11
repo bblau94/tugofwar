a5bee0a5f81a61d613241cc94dede1a3b079d498
bblau94 <benblau94@gmail.com>
2016-02-10 21:49:28 -0500

    Initial project build

---
+++ src/main/java/com/tugofwar/Tugofwar2Application.java
@@ -0,0 +1,12 @@
+package com.tugofwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tugofwar2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tugofwar2Application.class, args);
+	}
+}
